2505950412bbd613087e6bc9c124371ce559050b
goldencat-tom <bluebird6922@gmail.com>
2021-12-28 14:28:31 +0900

    usermenu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,50 +1,20 @@
-import { Link as RouterLink } from "react-router-dom";
-import {
-  AppBar,
-  Button,
-  Toolbar,
-  IconButton,
-  Link as MatLink,
-} from "@mui/material";
-import { useTheme } from "@emotion/react";
-import ThemeSwitch from "../ThemeSwitch";
-import ConnectWalletButton from "../ConnectWalletButton";
-import { useThemeManager } from "../../state/user/hooks";
-import "./style.css";
-
-const LinkButton = ({ to, label }: { to: string; label: string }) => {
-  return (
-    <MatLink
-      component={RouterLink}
-      to={to}
-      underline="none"
-      variant="h6"
-      color="white"
-      sx={{
-        m: 1.5,
-        "&:hover": {
-          color: "primary.main",
-        },
-      }}
-    >
-      {label}
-    </MatLink>
-  );
-};
+import { AppBar, Toolbar, IconButton } from '@mui/material'
+import { useTheme } from '@emotion/react'
+import { useThemeManager } from 'state/user/hooks'
+import ThemeSwitch from '../ThemeSwitch'
+import LinkButton from './LinkButton'
+import UserMenu from './UserMenu'
+import './style.css'
 
 export default function Header() {
-  const theme = useTheme();
-  const [isDark, toggleTheme] = useThemeManager();
-  console.log("theme", theme);
+  const theme = useTheme()
+  const [isDark, toggleTheme] = useThemeManager()
+  console.log('theme', theme)
 
   return (
-    <AppBar position="fixed" style={{ padding: "0 20px" }}>
+    <AppBar position="fixed" style={{ padding: '0 20px' }}>
       <Toolbar disableGutters={true} className="toolbarRoot">
-        <IconButton
-          color="inherit"
-          aria-label="Open drawer"
-          className="menuButton"
-        ></IconButton>
+        <IconButton color="inherit" aria-label="Open drawer" className="menuButton"></IconButton>
         <div className="menubar">
           <LinkButton to="/" label="Home" />
           <LinkButton to="/help" label="How it works" />
@@ -54,12 +24,9 @@ export default function Header() {
           <LinkButton to="/contact" label="Contact" />
           <LinkButton to="/login" label="Login" />
           <ThemeSwitch checked={isDark} onChange={toggleTheme}></ThemeSwitch>
-          <ConnectWalletButton variant="contained" className="connectButton" />
-          {/* <Button variant="contained" className="connectButton">
-            Connect Wallet
-          </Button> */}
+          <UserMenu />
         </div>
       </Toolbar>
     </AppBar>
-  );
+  )
 }
